db4f2b9b73fdc3a032b66714d24d3fe5f0e9be51
Atul <80565891+Atul-dubal@users.noreply.github.com>
2021-04-23 20:45:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="atul,atul dubal">
 
   <title>ATUL D. DUBAL</title>
   <link rel="stylesheet" href="style.css">
