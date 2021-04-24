5969e84857b2d0829bead69b7668c94c7d3862ac
Atul <80565891+Atul-dubal@users.noreply.github.com>
2021-04-24 14:44:16 +0530

    Create skill.php

---
+++ skill.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+<body>
+    <header><img id="menu" src="menu.png"> <img align="right" id="menu1" src="grampanchayat.jpg">
+  <div id="container1">
+    <div id="list">
+    <h2> <a href="index.php">HOME</a></h2>
+      <h2><a href="Gallery.php">GALLERY</a></h2>
+      <h2><a href="About.php">ABOUT</a></h2>
+         <h2><a href="Skill.php">SKILLS</a></h2>
+    </div>
+    </div>
+    </header>
+  <h3><marquee>माझे गाव खुटबाव
+  </marquee></h3>
+  <br>
+  <p style="   width: 100%;height: 50px;text-align:center;font-size: 40px;color: darkred;font-family: fantasy;  "><b><u><i>Young Talent</i></u></b></p>
+  <b>-Atul Dubal</b>
+  <p style="font-size: 20px">नमस्कार,मी अतुल डुबल आपल्या समोर ही Website (<a href="https://atuldubal.herokuapp.com"style="color:blue";>https://atuldubal.herokuapp.com</a>) आणत आहे.हि Website तुमच्या समोर आणण्यामागे अनेक कारणे आहेत. त्यापैकी एक असे कि प्रत्येक मुलाच्या अंगात काही गुण असतात पण ते गुण योग्य रित्या सादर करता येत नाहीत व त्यामुळे त्याचे गुण दबून राहतात. तर मी या Website च्या माध्यमातून आपल्या गावातील विविध मुलांना आपले गुण सादर करण्यासाठी प्रोत्साहित करू इच्छितो व त्यांचे विचार लोकांसमोर मांडू इच्छीतो.</p>
+<div id="box">
+  <img src="kavita.jpg" style="width: 80px;height: 80px; border-radius: 10px ;margin-top: 10px; margin-left: 40px;">
+    <img src="shayari.jpg" style="width: 80px;height: 80px;margin-top: 10px;margin-left: 100px;">
+       <img src="speech.jpg" style="width: 80px;height: 80px;margin-top: 10px;margin-left:40px;border: 1px solid black;border-radius: 10px;">
+</div>
+
+<script src="main.js"></script>
+</body>
+</html>
