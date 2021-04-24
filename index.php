007a3d5a7ab433ace1fd991b6c068f1bed48ffd1
Atul <80565891+Atul-dubal@users.noreply.github.com>
2021-04-24 14:42:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,125 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-</head>
-<link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="keywords" content="atul,dubal2206">
+ <meta name="decription" content="for about khutbav village">
+ <meta name="author" content="atul dubal">
+  <title>ATUL D. DUBAL</title>
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
-<body>
-    <header><img id="menu" src="menu.png"> <img align="right" id="menu1" src="grampanchayat.jpg">
+  <link rel="icon" href="Atul.jpg">
+  <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+</head>
+
+<body >
+  <header><img id="menu" src="menu.png"> <img align="right" id="menu1" src="grampanchayat.jpg">
   <div id="container1">
     <div id="list">
     <h2> <a href="index.php">HOME</a></h2>
       <h2><a href="Gallery.php">GALLERY</a></h2>
       <h2><a href="About.php">ABOUT</a></h2>
-         <h2><a href="Skill.php">SKILLS</a></h2>
+         <h2><a href="Skill.html">SKILLS</a></h2>
     </div>
     </div>
     </header>
   <h3><marquee>माझे गाव खुटबाव
   </marquee></h3>
   <br>
-  <p style="   width: 100%;height: 50px;text-align:center;font-size: 40px;color: darkred;font-family: fantasy;  "><b><u><i>Young Talent</i></u></b></p>
-  <b>-Atul Dubal</b>
-  <p style="font-size: 20px">नमस्कार,मी अतुल डुबल आपल्या समोर ही Website (<a href="https://atuldubal.herokuapp.com"style="color:blue";>https://atuldubal.herokuapp.com</a>) आणत आहे.हि Website तुमच्या समोर आणण्यामागे अनेक कारणे आहेत. त्यापैकी एक असे कि प्रत्येक मुलाच्या अंगात काही गुण असतात पण ते गुण योग्य रित्या सादर करता येत नाहीत व त्यामुळे त्याचे गुण दबून राहतात. तर मी या Website च्या माध्यमातून आपल्या गावातील विविध मुलांना आपले गुण सादर करण्यासाठी प्रोत्साहित करू इच्छितो व त्यांचे विचार लोकांसमोर मांडू इच्छीतो.</p>
-<div id="box">
-  <img src="kavita.jpg" style="width: 80px;height: 80px; border-radius: 10px ;margin-top: 10px; margin-left: 40px;">
-    <img src="shayari.jpg" style="width: 80px;height: 80px;margin-top: 10px;margin-left: 100px;">
-       <img src="speech.jpg" style="width: 80px;height: 80px;margin-top: 10px;margin-left:40px;border: 1px solid black;border-radius: 10px;">
-</div>
 
-<script src="main.js"></script>
+ <img src="khutbav.JPG" style="width:100%;height:300px;border: 2px solid black">
+<p>
+ <b>खुटबाव माहिती ::</b><br/>
+ <ol>
+ <li>खुटबाव हे भारताच्या महाराष्ट्र राज्याच्या सातारा जिल्ह्यातील माण तालुक्यातील एक गाव आहे</li>
+ <li>हा देश किंवा पश्चिम महाराष्ट्र प्रदेशाचा आहे</li>
+<li> ते पुणे विभागातील आहे</li>
+<li> हे जिल्हा मुख्यालय सातारा पासून पूर्वेकडे 90 कि.मी. अंतरावर आहे</li>
+ <li>मान पासून 26 किमी अंतरावर आहे.</li>
+<li> राज्याच्या राजधानी मुंबतील 275 कि.मी. खुटबाव पिन कोड 415508 आणि डाक मुख्यालय दहीवडी आहे</li>
+ <li>मोही (६ कि.मी.), आंद्रद (१२कि.मी.), मर्डी (१२ कि.मी.), सोकासन (१४ कि.मी.), इंजाबाव (१४ कि.मी) ही खुटबाव जवळची गावे आहेत.</li>
+ <li>खुटबाव पश्चिमेस मान तालुका, पश्चिमेस फलटण तालुका, उत्तरेकडे बारामती तालुका, पश्चिमेस खटाव तालुका आहे.</li>
+ <li>म्हसवड, फलटण, बारामती, माहुली ही खुटबाव जवळची शहरे आहेत</li>
+ <li>खुटबाव मराठीची लोकसंख्याशास्त्र ही स्थानिक भाषा आहे. </li>
+ </ol>
+</p>
+<p>
+  <b><i>खुटबाव स्थान ::</i></b>
+  <ol>
+  <li>तालुका नाव: मान</li>
+  <li>जिल्हा: सातारा</li>
+ <li> राज्य: महाराष्ट्र</li>
+ <li>प्रदेश: देश किंवा पश्चिम महाराष्ट्र</li>
+<li> विभाग: पुणे</li>
+  <li>भाषा: मराठी आणि सातारकर</li>
+  <li> टाइम झोन: IST (UTC + 5: 30)</li>
+ <li>उंची / उंची: 626 मीटर. समुद्र स्तरावरुन</li>
+ <li>  दूरध्वनी क्रमांक / एसटीडी कोड: 02165</li>
+ <li> पिन कोड: 415508</li>
+ <li> डाकघर नाव: दहिवडी </li>
+  </ol>
+</p>
+<p>
+ <b> जवळपास रेल्वे स्थानके ::</b>
+ <ul type="square">
+<li>  बारामती- 41 किमी
+  <br/>
+<li>  कटफल- 50 किमी
+  <br/>
+<li> भिगवण- 59 किमी
+  <br/>
+<li>सिरसुफळ - 60 किमी
+  </ul>
+  </p>
+  <p>
+ <b>जवळील विमानतळ ::</b>
+ <ul type="square" >
+ <li>  लोहेगाव विमानतळ- 131 कि.मि.
+   <br/>
+ <li> शोलापूर विमानतळ- 147 कि.मी.
+   <br/>
+ <li>  कोल्हापूर विमानतळ- 152 कि.मी.
+   <br/>
+<li>  लातूर विमानतळ- 219 कि.मी.
+   </ul>
+  </p>
+  <p>
+ <b> खुटबाव जवळील ठिकाणे ::</b>
+ <ul type="square">
+<li>  पंढरपूर- 75 कि.मी.
+  <br/>
+<li> सातारा- 83 कि.मी.
+  <br/>
+<li> मांडवा- 93 कि.मी.<br/>
+<li> पाचगणी  109 कि.मी.
+  <br/>
+  महाबळेश्वर- 125 कि.मी.
+  </ul>
+</p>
+<hr>
+<h1><marquee>THANKS FOR YOUR SUPPORT</marquee></h1>
+<hr>
+<article>
+  <p id="follow"><b><u>FOLLOW US</u></b></p>
+  <p><b>Term & Conditions</b></p>
+  <p>Contact No. :: 8108816901</p>
+  <p >E-mail :: atuldubal199@gmail.com</p>
+  <a href=" https:wa.me/+918108816901  ">
+   <img src="whatsApp.png"style="width:30px;height:30px"></a>
+   <a href=" https://www.instagram.com/atul_dubal_/ ">
+   <img src="insta.png" style="width: 30px;height:30px"></a>
+   <a href="alert('something went wrong')">
+   <img id="face" src="Facebook.png"style="width:30px;height:30px"></img></a>
+      <p>Powered By <span>Atul Dubal</span></p>
+   </article>
+   </article>
+</br>
+   <footer>
+     <article>&copy;copyright 2020/2021 The content in this website is prevented</article>
+  </footer>
+  <script src="main.js"></script>
 </body>
 </html>
+
